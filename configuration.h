9c3890d9ffdecfd367c66f0663542982102e030e
19UV <the19UV@gmail.com>
2021-06-11 16:57:30 -0400

    Renamed configurations, and moved essential definitions to header

--- configuration.h
+++ configuration.h
@@ -1,2 +1,7 @@
-// Should be able to decrease value, but may cause problems with recieving
-#define MAX_PACKET_SIZE 2097050
+// Should be able to decrease value, but may cause problems with recieving packets
+#define MAX_RECV_PACKET_SIZE 2097050
+#define MAX_SEND_PACKET_SIZE 32767
+
+// Constants
+#define RECV_BUFFER_ALLOCATE_FAILED 0
+#define SEND_BUFFER_ALLOCATE_FAILED 1
