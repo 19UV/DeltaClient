ed1517cf77f4b2c154a601af43bdf4064a092475
19UV <the19UV@gmail.com>
2021-06-11 17:34:16 -0400

    Added cpp-json library to build scripts

--- build_win_mingw.bat
+++ build_win_mingw.bat
@@ -2,6 +2,7 @@
 
 g++ src/main.cpp src/recv_buffer.cpp^
  -W -Wall^
+ -ID:/cpp-json-4.1/lib/include^
  -ID:/SDL2-2.0.14/include -LD:/SDL2-2.0.14/lib^
  -lmingw32 -lSDL2main -lSDL2 -lSDL2_net^
  -o DeltaClient.exe
